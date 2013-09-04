f449db1ef68f824f1ac8ef68d4ba2bfdcbb77e18
AJ Bahnken <aj@ajvb.me>
2013-09-04 08:19:10 -0700

    Cleaned up webpype.py; removed __init__ and print_options

--- src/webpype.py
+++ src/webpype.py
@@ -1,21 +1,17 @@
+'''
+The WebPype Class file. WebPype follows the spec of WebPipes ( webpipes.org )
+
+'''
 import json
 import urllib2
 
 class WebPype(object):
-
-    def __init__(self):
-        pass
 
     def options(self, url):
         request = urllib2.Request(url)
         request.get_method = lambda: 'OPTIONS'
         resp = urllib2.urlopen(request)
         return resp.read()
-
-    def print_options(self, url):
-        resp = self.options(url)
-        print resp
-        return
 
     def execute(self, url, inputs):
         data = json.dumps({'inputs': [inputs]})
